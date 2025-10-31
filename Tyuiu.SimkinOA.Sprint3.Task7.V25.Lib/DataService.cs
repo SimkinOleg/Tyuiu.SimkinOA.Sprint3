cb391b2a76d0db78e49f240c601636cacded6513
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-31 23:08:50 +0500

    Task7

---
+++ Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] valueArray = new double[len];
+
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                
+                if (x == 0)
+                {
+                    valueArray[count] = 0;
+                }
+                else
+                {
+                    double numerator = Math.Cos(x) + 4 * x;
+                    double denominator = 2 - Math.Sin(x) * 3 * x;
+
+                    
+                    if (Math.Abs(denominator) < 0.0001) 
+                    {
+                        valueArray[count] = 0;
+                    }
+                    else
+                    {
+                        valueArray[count] = Math.Round(numerator / denominator, 2);
+                    }
+                }
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint3.Task7.V25/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib;
+namespace Tyuiu.SimkinOA.Sprint3.Task7.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Симкин О.А. | Рппб-25-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич | Рппб-25-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("введите начальное значение отрезка: ");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите конечное значение отрезка: ");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetMassFunction(startValue, stopValue);
+
+            foreach (int number in result)
+            {
+                Console.WriteLine(number);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
