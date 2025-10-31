e41f0229c68dfba2d4152968511f8c3a2ff1c1b9
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-31 19:53:48 +0500

    Task3

---
+++ Tyuiu.SimkinOA.Sprint3.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SimkinOA.Sprint3.Task3.V11.Lib;
+namespace Tyuiu.SimkinOA.Sprint3.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string value = "qpddd.q";
+
+            DataService ds = new DataService();
+            string res = ds.ReplaceCharOnNum(value, 'q', '7');
+            Assert.AreEqual("7pddd.7", res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint3.Task3.V11/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SimkinOA.Sprint3.Task3.V11.Lib;
+namespace Tyuiu.SimkinOA.Sprint3.Task3.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Симкин О.А.| РППб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:  Оператор цикла foreach                                           *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич | РППб-25-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы q на цифру 7 в строке             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Строка: sqwreq vqwqq q");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "sqwreq vqwqq q";
+            DataService ds = new DataService();
+            string res = ds.ReplaceCharOnNum(value, 'q', '7');
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
