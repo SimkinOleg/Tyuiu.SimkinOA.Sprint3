983b3fabdf3c349eaa50f2ed4f9cf8526965924c
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-31 22:29:20 +0500

    Task5

---
+++ Tyuiu.SimkinOA.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.SimkinOA.Sprint3.Task5.V20.Lib;
+namespace Tyuiu.SimkinOA.Sprint3.Task5.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Симкин О.А. | Рппб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич | Рппб-25-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя вложенный цикл, которая вычисляет сумму    *");
+            Console.WriteLine("* ряда по формуле, при x = 5                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 6;
+
+            Console.WriteLine("Переменная Х = " + x);
+            Console.WriteLine("Старт шага первой суммы = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
