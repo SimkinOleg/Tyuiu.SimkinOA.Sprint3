4ac00b4a25c741e5ec995a8fd64a936bbeda11ae
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-01 12:14:33 +0500

    Task7

--- Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib/DataService.cs
+++ Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib/DataService.cs
@@ -1,5 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+using tyuiu.cources.programming.interfaces.Sprint3;
+
 namespace Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib
 {
     public class DataService : ISprint3Task7V25
@@ -9,21 +11,13 @@ namespace Tyuiu.SimkinOA.Sprint3.Task7.V25.Lib
             Func<double, double> f = x => Math.Cos(x) + 4 * x / 2 - Math.Sin(x) * 3 * x;
 
             double step = 1;
-            double[] values = new double[(int)((stopValue - startValue) / step) + 1];
+            double[] values = new double[(stopValue - startValue) + 1]; 
 
             int i = 0;
             for (double x = startValue; x <= stopValue; x += step)
             {
-                
-                if (x == 0)
-                {
-                    values[i] = 1;
-                }
-                else
-                {
-                    values[i] = Math.Round(f(x), 2);
-                }
-                i++;
+                values[i] = Math.Round(f(x), 2);
+                i++; 
             }
 
             return values;
